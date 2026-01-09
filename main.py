1350f2c3426350248a83671769872cb88313b078
Aihika37 <aihika.dutta11@gmail.com>
2026-01-09 14:25:35 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,69 +1,68 @@
-import pandas as pd
-import yfinance as yf
 import numpy as np
+import yfinance as yf
+import pandas as pd
 import matplotlib.pyplot as plt
-import scipy.optimize as optimization
-stocks=['AAPL','WMT','TSLA','GE','AMZN','DB']
-start_date="2014-01-01"
-end_date="2024-01-01"
-NUM_OF_T_DAYS=252
-NUM_OF_PF=10000
-def download_data():
-    stocks_data={}
-    for stock in stocks:
-        ticker=yf.Ticker(stock)
-        stocks_data[stock]=ticker.history(start=start_date,end=end_date)['Close']
-    return (pd.DataFrame(stocks_data))
-def show_data(data):
-    data.plot(figsize=(10,5))
-    plt.show()
-def log_daily(data):
-    return ((np.log(data/data.shift(1)))[1:])
-def generate_portfolio(returns):
-    expected_return=returns.mean()*NUM_OF_T_DAYS
-    expected_return=((expected_return).to_numpy())
-    weights=[]
-    expected_returns_pf=[]
-    expected_risk_pf=[]
-    for _ in range(NUM_OF_PF):
-        weight=(np.random.rand(len(stocks)))
-        weight=weight/weight.sum()
-        weights.append(weight)
-        expected_returns_pf.append(np.dot(expected_return,weight))
-        expected_risk_pf.append(np.sqrt(np.dot(weight.T,np.dot((returns.cov()*NUM_OF_T_DAYS),weight))))
-    return (weights),(expected_returns_pf),(expected_risk_pf)
+from scipy.stats import norm
+pd.set_option("display.max_columns", None)
+r_f=0.05
+mon=12
+class capm:
+    def __init__(self,stocks,starting_date,ending_date):
+        self.stocks=stocks
+        self.starting_date=starting_date
+        self.ending_date=ending_date
+    def get_data(self):
+        data={}
+        for stock in self.stocks:
 
-def func(weights,data):
-    ret = np.sum(data.mean() * weights) * NUM_OF_T_DAYS
-    ris = np.sqrt(np.dot(weights.T, np.dot((data.cov() * NUM_OF_T_DAYS), weights)))
-    return -ret/ris
-def optimizer(wei,data):
-    constraint = {'type': 'eq', 'fun': lambda x: np.sum(x) - 1}
-    bound=tuple((0,1) for _ in range(len(stocks)))
-    min=optimization.minimize(fun=func,x0=wei[0],args=data,method='SLSQP',
-                          constraints=constraint,bounds=bound)
-    print(f"Optimal Portfolio:{min['x'].round(3)}")
-    ret = np.sum(data.mean() * min['x'].round(3)) * NUM_OF_T_DAYS
-    ris = np.sqrt(np.dot(min['x'].round(3).T, np.dot((data.cov() * NUM_OF_T_DAYS), min['x'].round(3))))
-    print(f"Risk:{ris},Return:{ret},Sharpe Ratio:{ret/ris}")
-    return ris,ret
-def plot_curve(ret,risk,optret,optris):
-    plt.figure(figsize=(10,6))
-    plt.scatter(risk,ret,c=ret/risk,marker='o')
-    plt.xlabel("Expected Volatility")
-    plt.ylabel("Expected Return")
-    plt.grid(True)
-    plt.colorbar(label="Sharpe Ratio")
-    plt.plot(optris,optret,'g*',markersize=20)
-    plt.show()
-dataset=(download_data())
-show_data(dataset)
-log_daily_returns=(log_daily(dataset))
-generate_portfolio(log_daily_returns)
-pweights,preturns,prisks=generate_portfolio(log_daily_returns)
-pweights=np.array(pweights)
-preturns=np.array(preturns)
-prisks=np.array(prisks)
-oris,oret=optimizer(np.array(pweights),log_daily_returns)
-plot_curve(np.array(preturns),np.array(prisks),oret,oris)
+            ticker=yf.Ticker(stock)
+            data[stock]=ticker.history(start=self.starting_date,end=self.ending_date)['Close']
+        return pd.DataFrame(data)
+    def arrange_data(self,r_data):
+        r_data['s_logdaily_ret']=np.log(r_data['s_returns']/r_data['s_returns'].shift(1))
+        r_data['m_logdaily_ret'] = np.log(r_data['m_returns'] / r_data['m_returns'].shift(1))
+        return r_data
+    def get_beta(self,da):
+       cova=np.cov(da['s_logdaily_ret'],da['m_logdaily_ret'])
+       return (cova[1][0]/cova[1][1])
+    def regression(self,da):
+        beta,alpha=np.polyfit(da['m_logdaily_ret'],da['s_logdaily_ret'],deg=1)
+        print(f"Beta from regression:{beta}")
+        expected_ret=r_f+beta*(da['m_logdaily_ret'].mean()*mon-r_f)
+        print(f"Expected Return:{expected_ret}")
+        self.plot(da,beta,alpha)
+    def plot(self,da,beta,alpha):
+        fig,axis=plt.subplots(1,figsize=(20,10))
+        axis.scatter(da['m_logdaily_ret'],da['s_logdaily_ret'],label="Data points")
+        axis.plot(da['m_logdaily_ret'],beta*(da['m_logdaily_ret'])+alpha,color='Red')
+        plt.title("Capital Asset Price Model")
+        plt.xlabel("Market Return",fontsize=18)
+        plt.ylabel("Stock Return")
+        plt.legend()
+        plt.grid(True)
+        plt.show()
 
+stoc=['IBM','^GSPC']
+s_d='2014-01-01'
+e_d='2024-01-01'
+c=capm(stoc,s_d,e_d)
+d=(c.get_data())
+d=d.resample('M').last()
+dat=pd.DataFrame({'s_returns':d[stoc[0]],'m_returns':d[stoc[1]]})
+data=c.arrange_data(dat)
+data=data[1:]
+print(f"Beta:{c.get_beta(data)}")
+c.regression(data)
+tick=yf.Ticker('IBM')
+s={}
+s['Price']=tick.history(start=s_d,end=e_d)['Close']
+s=pd.DataFrame(s)
+s=np.log(s/s.shift(1))
+s=s[1:]
+plt.hist(s,bins=700)
+m=s.mean()
+v=s.var()
+sig=np.sqrt(v)
+x=np.linspace(m-3*sig,m+3*sig,100)
+plt.plot(x,norm.pdf(x,m,sig))
+plt.show()
